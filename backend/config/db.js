727d1518a79f1679e5918cdcf425bdc835b7934f
fatimau <fatimauf1523@gmail.com>
2026-05-11 02:37:54 +0500

    fix mongodb env variable name

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio_builder');
-    console.log(`\x1b[32m✔ MongoDB Connected: ${conn.connection.host}\x1b[0m`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✔ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`\x1b[31m✘ Database connection error: ${error.message}\x1b[0m`);
-    process.exit(1); // Exit process with failure
+    console.error(`✘ Database connection error: ${error.message}`);
+    process.exit(1);
   }
 };
 
